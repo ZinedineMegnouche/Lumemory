831737ded9ac45f19d086a8ae039c430b09cd544
Zinedine MEGNOUCHE <megnouche.z@gmail.com>
2024-02-27 18:34:39 +0100

    Init du Projet
    avec autorisation homekit

---
+++ LumenMemory/LumenMemory/LumenMemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumenMemoryApp.swift
+//  LumenMemory
+//
+//  Created by Zinedine Megnouche on 27/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct LumenMemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
